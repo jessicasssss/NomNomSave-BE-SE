bd88e7b1e80fc7cfea6236ba5d3d69c43ae826ce
jessicasssss <2005jessicabella@gmail.com>
2025-05-31 23:54:49 +0700

    BE-4.1

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const productModel = require("../models/productModel");
 const roomModel = require("../models/roomModel");
 const moment = require("moment");
-const isUserInRoom = require('../utils/isUserInRoom');
+const isUserInRoom = require('../utils/roomUtils');
 
 exports.addProduct = (req, res) => {
   const { userId, teamId, ProductName, ExpiredDate, ProductCategory } =
