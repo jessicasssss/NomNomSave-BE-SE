5074af39abdde6024795e14453cfdff4026dec93
jessicasssss <2005jessicabella@gmail.com>
2025-05-31 21:20:12 +0700

    BE-3

--- models/db.js
+++ models/db.js
@@ -10,7 +10,12 @@ const db = mysql.createConnection({
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD || '', // Use empty string if DB_PASSWORD is not set
   database: process.env.DB_NAME, 
+  port: process.env.PORT,
   // waitForConnections: true, // Enable connection pooling
+  // ssl: {
+  //   rejectUnauthorized: true, // Disable SSL certificate validation
+  // },
+ 
 });
 
 db.connect((err) => {
